46977ca38f17a7f1867582bcb04503f7af780c36
JAW1 <Jakewelling@gmail.com>
2021-01-25 22:39:54 -0700

    Assignment 2

--- wwwroot/js/gradecalc.js
+++ wwwroot/js/gradecalc.js
@@ -1,20 +1,17 @@
-﻿//Jacob Welling Assignment 1
-
-var submit = document.getElementById("submitButton");
-//create click function
-submit.addEventListener("click", function () {
+﻿//Jacob Welling Assignment 2
+$("#submitButton").click(function () {
     //assign variables
     var assignmentWeight = .5
     var groupProjectWeight = .1
     var quizWeight = .1
     var examWeight = .2
     var intexWeight = .1
-
-    var assignmentPointsEarned = document.getElementById('assignments').value
-    var groupProjectPointsEarned = document.getElementById('groupProjects').value
-    var quizPointsEarned = document.getElementById('quizzes').value
-    var examPointsEarned = document.getElementById('exams').value
-    var intexPointsEarned = document.getElementById('intex').value
+    //Validation is located within the tags of GradeCalcPage.cshtml
+    var assignmentPointsEarned = $('assignments').value
+    var groupProjectPointsEarned = $('groupProjects').value
+    var quizPointsEarned = $('quizzes').value
+    var examPointsEarned = $('exams').value
+    var intexPointsEarned = $('intex').value
 
     var totalGrade = 0
     //calculate total grade
@@ -24,6 +21,7 @@ submit.addEventListener("click", function () {
     totalGrade = totalGrade + (examWeight * examPointsEarned)
     totalGrade = totalGrade + (intexWeight * intexPointsEarned)
     //display total grade
+    
     alert(totalGrade)
 
 
